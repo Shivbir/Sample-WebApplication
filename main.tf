b5e98d41f982512367bd52a18a9670e408d04fdd
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-05-30 01:17:25 -0400

    add

--- main.tf
+++ main.tf
@@ -67,3 +67,77 @@ resource "aws_route_table_association" "my_vpc_us_east_1b_public" {
     subnet_id = aws_subnet.public_us_east_1b.id
     route_table_id = aws_route_table.my_vpc_public.id
 }
+
+//creating launch config
+resource "aws_launch_configuration" "web" {
+  name_prefix = "web-"
+
+  image_id = "ami-0d5eff06f840b45e9" 
+  instance_type = "t2.micro"
+
+  security_groups = [ aws_security_group.allow_http.id ]
+  associate_public_ip_address = true
+
+  user_data = < /usr/share/nginx/html/index.html
+chkconfig nginx on
+service nginx start
+  USER_DATA
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+//load balancer
+resource "aws_security_group" "elb_http" {
+  name        = "elb_http"
+  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP through ELB Security Group"
+  }
+}
+
+resource "aws_elb" "web_elb" {
+  name = "web-elb"
+  security_groups = [
+    aws_security_group.elb_http.id
+  ]
+  subnets = [
+    aws_subnet.public_us_east_1a.id,
+    aws_subnet.public_us_east_1b.id
+  ]
+
+  cross_zone_load_balancing   = true
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+  }
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "80"
+    instance_protocol = "http"
+  }
+
+}
