55835a6d369bec73ba12a4513de85f68befea11a
Shivbir Kaur <shivbirkaur@amits-air.lan>
2021-05-30 01:02:36 -0400

    add

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 resource "aws_vpc" "my_vpc" {
   cidr_block       = "10.0.0.0/16"
   enable_dns_hostnames = true
